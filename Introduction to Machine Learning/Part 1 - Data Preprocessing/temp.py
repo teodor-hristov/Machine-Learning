0fc119b883c4c97ed9622109d52df26f87696833
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-12-11 22:09:13 +0200

    Initial commit

--- Introduction to Machine Learning/Part 1 - Data Preprocessing/temp.py
+++ Introduction to Machine Learning/Part 1 - Data Preprocessing/temp.py
@@ -29,4 +29,4 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2, random_sta
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+    X_test = sc_X.transform(X_test)"""
